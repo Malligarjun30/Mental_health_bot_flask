9725e28b80e23aa36438ac787a57baae6c56ae8e
Malligarjun30 <111998125+Malligarjun30@users.noreply.github.com>
2025-03-05 22:08:35 +0530

    Update run.py

--- run.py
+++ run.py
@@ -7,27 +7,27 @@ app = create_app()
 # if __name__ == '__main__':
 #     app.run()
 
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
-# db = SQLAlchemy()
-# login_manager = LoginManager()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-# def create_app():
-#     app = Flask(__name__)
-#     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///users.db' 
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     app.config['SECRET_KEY'] = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///users.db' 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
-#     db.init_app(app)
-#     login_manager.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
-#     with app.app_context():
-#         db.create_all()  # Create tables if they don't exist
+    with app.app_context():
+        db.create_all()  # Create tables if they don't exist
 
-#     return app
+    return app
 
 # Health check endpoint
 @app.route('/health')
